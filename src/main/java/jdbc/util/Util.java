c9790ac56ab0c91f9d9eb1d03505ec10106c4489
nitropasha <djioeff@gmail.com>
2024-08-02 10:18:48 +0500

    2024

--- src/main/java/jdbc/util/Util.java
+++ src/main/java/jdbc/util/Util.java
@@ -2,12 +2,6 @@ package jdbc.util;
 
 import java.sql.Connection;
 
-import jdbc.model.User;
-import org.hibernate.Session;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.SessionFactory;
-import org.hibernate.service.ServiceRegistry;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -28,31 +22,3 @@ public class Util {
 
     }
 }
-//public class Util {
-//    // реализуйте настройку соеденения с БД
-//    private static SessionFactory sessionFactory;
-//    public static SessionFactory getConnection() {
-//
-//        Configuration configuration = new Configuration();
-//        // Настройка параметров подключения к базе данных
-//        configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-//        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
-//        configuration.setProperty("hibernate.connection.username", "root");
-//        configuration.setProperty("hibernate.connection.password", "goldrain");
-//        // Настройка диалекта Hibernate
-//        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-//        // Дополнительные параметры Hibernate
-//        configuration.setProperty("hibernate.show_sql", "true");
-//        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
-//        // Добавление аннотированных классов (Entity)
-//        configuration.addAnnotatedClass(User.class);
-//        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-//                .applySettings(configuration.getProperties()).build();
-//
-//        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-//
-//
-//        return sessionFactory;
-//
-//    }
-//}
